e9bef3a39d38e323413b4f00dda3e29e7a14d3e9
Sheldon Henriques <32040506+sheldonhenriques@users.noreply.github.com>
2025-07-26 17:25:53 -0700

    Update LoginButton.tsx

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -30,7 +30,7 @@ export default function LoginButton() {
         </div>
         <button
           onClick={signOut}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-green-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         >
           Sign Out
         </button>
